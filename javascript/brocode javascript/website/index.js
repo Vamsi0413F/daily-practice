9110c34ce9d770d2e1730741cfd6b3ab4dbc9e56
Vamsi0413F <cb.sc.u4cse25346@cb.students.amrita.edu>
2026-05-29 07:46:03 +0530

    all remaining html files

---
+++ javascript/brocode javascript/website/index.js
@@ -0,0 +1,31 @@
+// console.log(`Hello`);
+// console.log(`wigga`);
+
+// window.alert(  `this is an alert`);
+// window.alert(  `you are my wigga`);
+// document.getElementById("Number1").textContent = `Hello`;
+// document.getElementById("Number2").textContent = ` my Wigga`;
+//let age = 25;
+// let gpa = 8.5;
+// let price = 99.99;
+//let firstName="Raghammudi";
+// let email=`abc@gmail.com`;
+
+// console.log(typeof email);
+// console.log(typeof age);
+// console.log(typeof price);
+// console.log(typeof gpa);
+// console.log(typeof firstName);
+
+// console.log(`you are ${age} years old`);
+// console.log(`the final price is $${price}`)
+// console.log(`your gpa is ${gpa}`);
+// console.log(`your first name is ${firstName}`)
+
+// let online = true;
+// console.log(typeof online);
+// console.log(`Bro is online ${online}`);
+// document.getElementById("p1").textContent = `your nae is ${firstName}`;
+// document.getElementById("p2").textContent = `your age is ${age}`;
+// document.getElementById("p3").textContent = `Are you Studying in School : ${online}`;
+console.log(1/3);
